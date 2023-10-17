a6b744f195da6c74ef9c6afb82764fed4e527cb2
xin700 <992950082@qq.com>
2023-10-17 19:51:57 +0800

    submit the FirstWeekAssignments

---
+++ FirstWeekAssignments/任务五/segment/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/xin/HustFiles/狼牙战队/RM_assignments/FirstWeekAssignments/任务五/segment/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/Users/xin/HustFiles/狼牙战队/RM_assignments/FirstWeekAssignments/任务五/segment/src/segment.cpp" "CMakeFiles/main.dir/src/segment.cpp.o" "gcc" "CMakeFiles/main.dir/src/segment.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
